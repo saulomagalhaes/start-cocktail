a85890e1fe2851217bb8cccdc9e080b69cd5e8b3
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-27 19:55:02 -0300

    feat: :sparkles: adiciona evento de click nos botões de categoria

--- src/pages/Home/CategoryButtons/index.tsx
+++ src/pages/Home/CategoryButtons/index.tsx
@@ -1,4 +1,6 @@
 import { Button, Grid, Typography } from '@mui/material';
+import { useContext } from 'react';
+import { CocktailContext } from '../../../contexts/cocktail';
 
 export function CategoryButtons() {
   const categories = [
@@ -14,6 +16,13 @@ export function CategoryButtons() {
     'Beer',
     'Soft Drink',
   ];
+
+  const { setCocktailByCategory } = useContext(CocktailContext);
+
+  const handleClick = (category: string) => {
+    setCocktailByCategory(category);
+  };
+
   return (
     <>
       <Typography variant="h5" sx={{ marginTop: 1, marginLeft: 1 }}>
@@ -23,6 +32,7 @@ export function CategoryButtons() {
         {categories.map((category) => (
           <Grid key={category} item xs={2}>
             <Button
+              onClick={() => handleClick(category)}
               size="small"
               variant="contained"
               sx={{ width: '90%', margin: 1 }}
