246fa4df93c34e41053420340e6f641d4cd048c3
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-27 10:03:28 -0300

    refactor: :recycle: modifica o componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,11 @@
 import { AppBar, Box, Toolbar } from '@mui/material';
 import logo from '../../assets/startrecipe.png';
+
 export function Header() {
   return (
     <AppBar position="static" color="primary" component="header">
       <Toolbar>
-        <Box height="80px">
-          <img src={logo} alt="" style={{ height: '100%' }} />
-        </Box>
+        <Box component="img" src={logo} alt="Start Recipe" height="5rem" />
       </Toolbar>
     </AppBar>
   );
