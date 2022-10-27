5cab3ee52547b0402c26162a2bd4079174d601ff
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-27 10:05:09 -0300

    feat: :sparkles: adiciona um Container e a Lista de coquetéis a página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,14 @@
+import { Container } from '@mui/material';
 import { Header } from '../../components/Header';
+import { ListCocktails } from './ListCocktails';
 
 export function Home() {
   return (
     <>
       <Header />
+      <Container maxWidth="xl">
+        <ListCocktails />
+      </Container>
     </>
   );
 }
