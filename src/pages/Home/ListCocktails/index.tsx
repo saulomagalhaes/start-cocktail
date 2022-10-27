b4d3b054b8c00fe7b935b87aa1ac363d324d6f0c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-27 10:06:44 -0300

    feat: :sparkles: cria componente para listar os coquetéis provenientes da API

---
+++ src/pages/Home/ListCocktails/index.tsx
@@ -0,0 +1,48 @@
+import { Box, CircularProgress, Grid } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { ICocktail } from '../../../interfaces/ICocktail';
+import { fetchCocktails } from '../../../services/axios';
+import { CocktailCard } from './CocktailCard';
+
+export function ListCocktails() {
+  const [cocktails, setCocktails] = useState<ICocktail[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const handleFetchCocktails = async () => {
+    const response = await fetchCocktails();
+    setCocktails(response);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handleFetchCocktails();
+  }, []);
+  return (
+    <Grid container my={2} spacing={2}>
+      {loading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            width: '100vw',
+            height: '100vh',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <CircularProgress size={100} />
+        </Box>
+      ) : (
+        cocktails.map((cocktail) => (
+          <Grid
+            item
+            xs={3}
+            key={cocktail.idDrink}
+            sx={{ display: 'flex', justifyContent: 'center' }}
+          >
+            <CocktailCard cocktail={cocktail} />
+          </Grid>
+        ))
+      )}
+    </Grid>
+  );
+}
