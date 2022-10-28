0fa19f6777226c9ae8067f8142ba4ee0800e0245
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2022-10-28 15:12:28 -0300

    feat: :sparkles: cria rota para detalhes de um coquetel

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import { CocktailDetail } from './pages/CocktailDetail';
 import { Home } from './pages/Home';
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/cocktail/:id" element={<CocktailDetail />} />
     </Routes>
   );
 }
